2dd3fdef8b83e74adb5cb62ac903e87cfb8f6b6c
shockbob <shockbob@gmail.com>
2022-01-11 08:49:50 -0700

    More changes

--- reversi/src/reversi/extra.clj
+++ reversi/src/reversi/extra.clj
@@ -1,5 +1,6 @@
 (ns reversi.extra
   (:gen-class))
+
 (def blank \-)
 (def black \B)
 (def white \W)
@@ -7,6 +8,26 @@
 (defn get-dirs [] dirs)
 (def blank-grid {})
 (def opposite {white black black white})
+
+(defn char-to-entry [ch i]
+  (if (or (= ch \W) (= ch \B))
+    [{:row (quot i 8) :col (mod i 8)} ch]
+    nil))
+
+(defn str-to-grid [s]
+  (apply hash-map
+         (mapcat
+          (fn [i] (char-to-entry (get s i) i))
+          (range (count s)))))
+
+(defn max-by [keyfn coll]
+  (if (empty? coll)
+    nil
+    (let [grouped (group-by keyfn coll)
+          keys (keys grouped)
+          mx (apply max keys)]
+      (grouped mx))))
+
 (def value-grid-x [[:corner :corner-adj :side :side :side :side :corner-adj :corner]
                    [:corner-adj :corner-adj :side-adj :side-adj :side-adj :side-adj :corner-adj :corner-adj]
                    [:side :side-adj :center :center :center :center :side-adj :side]
@@ -18,21 +39,27 @@
 
 (def value-map {:corner 40 :corner-adj 1 :center 10 :side 20 :side-adj 1})
 
-
 (defn vec-grid [grid]
   (vec
-    (map
-      vec
-      (partition 8
-                 (for [row (range 8) col (range 8)]
-                   (grid {:row row :col col} \-))))))
+   (map
+    vec
+    (partition 8
+               (for [row (range 8) col (range 8)]
+                 (grid {:row row :col col} \-))))))
 
 (defn printable [grid]
-  (apply str (interpose "\r\n"
-                        (map (partial apply str) (vec-grid grid)))))
+  (apply str (interpose
+              "\r\n"
+              (map (partial apply str) (vec-grid grid)))))
 
-(defn printable-grids [grid]
-  (interpose "\r\n\r\n" (map printable grid)))
+(defn printable-grids [grids]
+  (interpose "\r\n\r\n" (map printable grids)))
+
+(defn printable-single-line [grid]
+  (apply str (flatten (vec-grid grid))))
+
+(defn printable-grids-single-line [grids]
+  (interpose "\r\n\r\n" (map printable-single-line grids)))
 
 (defn in-range? [val]
   (< -1 val 8))
@@ -43,7 +70,9 @@
 (defn make-diffs
   ([dir] (make-diffs dir 1))
   ([{dr :dr dc :dc} start]
-   (map (fn [mult] {:dr (* dr mult) :dc (* dc mult)}) (range start 9))))
+   (map
+    (fn [mult] {:dr (* dr mult) :dc (* dc mult)})
+    (range start 9))))
 
 (defn make-coords [{r :row c :col} diffs]
   (map (fn [{dr :dr dc :dc}] {:row (+ r dr) :col (+ c dc)}) diffs))
@@ -71,19 +100,23 @@
         stable-size (count-stables colors color)]
     (take (min stable-size max-size) forward-coords)))
 
+(defn adjust-chunk-size [chunk-size]
+  (cond
+    (= chunk-size 8) 8
+    (= chunk-size 1) 1
+    :else (dec chunk-size)))
+
 (defn stable-section
   ([grid forward-dir edge-coords color]
    (stable-section grid forward-dir edge-coords color 8 []))
   ([grid forward-dir [f & r] color mx coll]
-   (if (or (nil? f)(zero? mx))
-       coll
-       (let [chunk (stable-chunk grid f forward-dir color mx)
-             mx (dec (count chunk))]
-         (recur grid forward-dir r color mx (concat coll chunk))))))
+   (if (or (nil? f) (zero? mx))
+     coll
+     (let [chunk (stable-chunk grid f forward-dir color mx)
+           mx (adjust-chunk-size (count chunk))]
+       (recur grid forward-dir r color mx (concat coll chunk))))))
 
-
-
-(defn stables [grid corner-coord color]
+(defn stables-from-corner [grid corner-coord color]
   (let [port-dir (get-in corners [corner-coord :port])
         forward-dir (get-in corners [corner-coord :forward])
         valid-edges (get-valid-coords-in-dir corner-coord port-dir 0)
@@ -91,18 +124,23 @@
     stables))
 
 (defn stables-on-grid [grid color]
-  (mapcat (fn [corner-coord] (stables grid corner-coord color))
-     (keys corners)))
-     
+  (let [stable-vec (mapcat
+                    (fn [corner-coord] (stables-from-corner grid corner-coord color))
+                    (keys corners))]
+    (set stable-vec)))
+
 (defn neighbors [coord]
   (filter in-range-coord?
-         (make-coords coord (get-dirs))))
+          (make-coords coord (get-dirs))))
 
 (defn make-grid [grid-data]
   (reduce
    (fn [g [r c val]] (assoc g {:row r :col c} val))
    blank-grid
    grid-data))
+
+(defn get-best-stable-change [new-grid-map count-stables]
+  (max-by (fn [move] (- (count (stables-on-grid (new-grid-map move) (move :color)  count-stables))) (keys new-grid-map))))
 
 (def start-grid
   (make-grid [[3 3 black] [4 4 black] [3 4 white] [4 3 white]]))
@@ -118,20 +156,35 @@
 (defn find-color [grid color]
   (map first
        (filter
-         (fn [[k v]] (= v color))
-         grid)))
+        (fn [[k v]] (= v color))
+        grid)))
 
-(defn countem [[f & r] color total]
-  (cond
-    (nil? f) 0
-    (= color f) total
-    (= (opposite color) f) (countem r color (inc total))
-    :else 0))
+(defn count-flips-x [colors color]
+  (reduce (fn [[state count] e]
+            (cond
+              (= state :completed) [state count]
+              (= state :found-opposite)
+              (cond
+                (= e color) [:completed count]
+                :else [state (inc count)])
+              (= state :looking-for-opposite)
+              (cond
+                (= e (opposite color))  [:found-opposite 1]
+                :else [:invalid 0])
+              :else [:invalid 0]))
+          [:looking-for-opposite 0]
+          colors))
 
-(defn count-flips [colors color]
-  (countem colors color 0))
+(defn count-flips
+  ([colors color] (count-flips colors color 0))
+  ([[f & r] color total]
+   (cond
+     (nil? f) 0
+     (= color f) total
+     (= (opposite color) f) (recur r color (inc total))
+     :else 0)))
 
-(defn make-move [grid [coord flips] color]
+(defn make-move [grid {coord :move flips :flips color :color}]
   (let [new-grid (assoc grid coord color)
         new-grid (reduce (fn [g c] (assoc g c color)) new-grid flips)]
     new-grid))
@@ -153,79 +206,132 @@
         blanks (distinct (mapcat (partial blank-neighbors grid) opps))]
     blanks))
 
+(defn create-moves [grid move-coords color]
+  (map
+   (fn [mv] {:color color :move mv :flips (get-all-flips grid color mv)})
+   move-coords))
+
+(defn get-valid-moves [moves]
+  (filter
+   (fn [{flips :flips}] (seq flips))
+   moves))
+
 (defn get-all-moves [grid color]
-  (let [moves (possible-moves grid color)
-        flips (map (fn [coord] [coord (get-all-flips grid color coord)]) moves)
-        flips (filter (fn [[coord flips]] (not (empty? flips))) flips)]
-    flips))
+  (let [possible-moves (possible-moves grid color)
+        all-moves (create-moves grid possible-moves color)
+        valid-moves (get-valid-moves all-moves)]
+    valid-moves))
 
 (defn scores [grid]
   (frequencies (vals grid)))
 
-(defn play-next-move [grid color chooser]
-  (let [all-moves (get-all-moves grid color)
-        best-move (chooser all-moves grid)
-        grid (if (empty? all-moves) grid (make-move grid best-move color))]
-    grid))
+(defn add-reversed [mp key coll]
+  (reduce (fn [mp e] (assoc mp e key)) mp coll))
 
-(defn max-by [keyfn [f & r]]
-  (if (nil? f)
+(defn make-normalized-groups [keyfn coll]
+  (if (empty? coll)
     nil
-   (first
-    (reduce
-     (fn [[max-e max-fe] e]
-       (let [fe (keyfn e)]
-         (if (> fe max-fe)
-           [e fe]
-           [max-e max-fe])))
-     [f (keyfn f)]
-     r))))
+    (let [grouped (group-by keyfn coll)
+          keys (sort > (keys grouped))
+          converter-map (apply hash-map (interleave keys (range)))]
+      (reduce
+       (fn [mp [k v]] (add-reversed mp (converter-map k) v))
+       {}
+       grouped))))
+
 
 (def upper-left  {:row 0 :col 0})
 (def upper-right {:row 0 :col 7})
 (def lower-right {:row 7 :col 7})
 (def lower-left  {:row 7 :col 0})
-(def corners-map
-  {{ :row 0 :col 1 } upper-left, { :row 1 :col 0 } upper-left, { :row 1 :col 1 } upper-left,
-   { :row 6 :col 7 } lower-right, { :row 7 :col 6 } lower-right, { :row 6 :col 6 } lower-right,
-   { :row 0 :col 6 } upper-right, { :row 1 :col 6 } upper-right, { :row 1 :col 7 } upper-right,
-   { :row 6 :col 0 } lower-left, { :row 6 :col 1 } lower-left, { :row 7 :col 1 } lower-left})
+(def corner-map
+  {{:row 0 :col 1} upper-left, {:row 1 :col 0} upper-left, {:row 1 :col 1} upper-left,
+   {:row 6 :col 7} lower-right, {:row 7 :col 6} lower-right, {:row 6 :col 6} lower-right,
+   {:row 0 :col 6} upper-right, {:row 1 :col 6} upper-right, {:row 1 :col 7} upper-right,
+   {:row 6 :col 0} lower-left, {:row 6 :col 1} lower-left, {:row 7 :col 1} lower-left})
+
+(defn square-type [{row :row col :col}]
+  (get-in value-grid-x [row col]))
 
 (defn value-calculate [mv]
-  (value-map (get-in value-grid-x [(mv :row)(mv :col)])))
+  (value-map (square-type mv)))
 
-(defn value-move [[mv flips]]
+(defn value-move [{mv :move flips :flips}]
   (reduce + (map value-calculate (conj flips mv))))
 
 (defn value-chooser-2 [moves grid]
   (max-by value-move moves))
 
+(defn find-by-square-type [type]
+  (fn [moves] (filter (fn [mv] (= type (square-type (mv :move)))) moves)))
+
 (defn get-value [grid mv]
-  (let [corner-value (corners-map mv)]
+  (let [corner-value (corner-map mv)]
     (if (or (nil? corner-value) (nil? (grid corner-value)))
       (value-calculate mv)
       (value-map :corner))))
 
 (defn value-chooser [moves grid]
-  (max-by (fn [[mv flips]] (value-calculate mv)) moves))
+  (max-by (fn [{mv :move flips :flips}] (value-calculate mv)) moves))
 
 (defn value-chooser-enh [moves grid]
-  (max-by (fn [[mv flips]] (reduce + (map (partial get-value grid) (conj flips mv)))) moves))
+  (max-by (fn [{mv :move flips :flips}] (get-value grid mv)) moves))
+
+(defn screw-opp-chooser [moves grid])
+
+(defn value-chooser-enh2 [moves grid]
+  (max-by (fn [{mv :move flips :flips}] (reduce + (map (partial get-value grid) (conj flips mv)))) moves))
 
 (defn size-chooser [moves grid]
-  (max-by (fn [[mv flips]] (count flips)) moves))
+  (max-by (fn [{mv :move flips :flips}] (count flips)) moves))
 
-(defn rand-xth [c grid] (if (empty? c) nil (rand-nth c)))
+(defn rand-plus-chooser [moves grid]
+  (if (empty? moves)
+    nil
+    (let [testers [(find-by-square-type :corner) (find-by-square-type :side) (find-by-square-type :center)]
+          results (first (remove empty? (map (fn [x] (x moves)) testers)))
+          best-moves (if (empty? results) (value-chooser-enh moves grid) results)]
+      best-moves)))
+
+(defn make-new-grids [grid moves]
+  (reduce (fn [m move] (assoc m move (make-move grid move))) {} moves))
+
+(defn rand-chooser [c grid] (shuffle c))
+(defn stables-chooser [moves grid]
+  (if (empty? moves)
+    nil
+    (let [existing-stables (stables-on-grid grid ((first moves) :color))
+          count-stables (count existing-stables)
+          new-grid-map (make-new-grids grid moves)
+          [move score] (get-best-stable-change new-grid-map count-stables)
+          best-move (if (> score 0) move)]
+      best-move)))
+
+(defn last-move-chooser [moves grid]
+  (let [new-grid-map (make-new-grids grid moves)
+        filtered-moves (filter
+                        (fn [[mv new-grid]] (nil? ((scores new-grid) (opposite (mv :color)))))
+                        new-grid-map)]
+    filtered-moves))
 
 (defn test-summary [all-scores]
   (reduce
-   (fn [res score] (if (> (score black 0) (score white 0))
-                     (assoc res black (inc (res black 0)))
-                     (assoc res white (inc (res white 0)))))
+   (fn [res score] (cond
+                     (> (score black 0) (score white 0))  (assoc res black (inc (res black 0)))
+                     (> (score white 0) (score black 0))  (assoc res white (inc (res white 0)))
+                     :else (assoc res "tie" (inc (res "tie" 0)))))
    {}
    all-scores))
 
-(defn addem [a b] (+ a b))
+(defn play-next-move
+  ([grid color chooser]
+   (play-next-move grid color chooser shuffle))
+  ([grid color chooser modifier]
+   (let [all-moves (get-all-moves grid color)
+         best-moves (chooser (modifier all-moves) grid)
+         best-move (rand-nth best-moves)
+         grid (if (empty? all-moves) grid (make-move grid best-move))]
+     grid)))
 
 (defn play-game [grid grids black-chooser white-chooser]
   (let [black-grid (play-next-move grid black black-chooser)
@@ -236,13 +342,12 @@
 
 (defn dotest [n]
   (let [games (take n (repeatedly
-                       #(play-game start-grid [] rand-xth rand-xth)))
+                       #(play-game start-grid [] value-chooser-enh rand-plus-chooser)))
         all-scores (map scores (map last games))]
     (test-summary all-scores)))
-
+;
 ;(println (dotest 1000))
 
-;(def grids (play-game start-grid [] size-chooser value-chooser-enh))
+;(def grids (play-game start-grid [] rand-xth rand-plus-chooser))
 ;(println (printable-grids grids))
 ;(println (scores (last grids)))
-;
--- reversi/test/reversi/core_test.clj
+++ reversi/test/reversi/core_test.clj
@@ -2,24 +2,30 @@
   (:require [clojure.test :refer :all]
             [reversi.extra :refer :all]
             [reversi.core :refer :all]))
-(def test-stable-grid (make-grid [[0 0 white][0 1 white] [0 2 white]
-                                  [1 0 white][1 1 white]]))
+(def test-grid (make-grid (for [i (range 8) j (range 2)] [i j white])))
+
+(deftest test-stables-on-grid-1
+  (testing
+   (is (= 16 (count (stables-on-grid test-grid white))))))
+
+(def test-stable-grid (make-grid [[0 0 white] [0 1 white] [0 2 white]
+                                  [1 0 white] [1 1 white]]))
 
 (deftest test-stable-chunk-1
   (testing
-    (is (= [{:row 0 :col 0} {:row 0 :col 1} {:row 0 :col 2}]
-           (stable-chunk test-stable-grid {:row 0 :col 0} {:dr 0 :dc 1} white 8)))))
+   (is (= [{:row 0 :col 0} {:row 0 :col 1} {:row 0 :col 2}]
+          (stable-chunk test-stable-grid {:row 0 :col 0} {:dr 0 :dc 1} white 8)))))
 (deftest make-diffs-1
   (testing "make-diffs works"
     (is (= [{:dr 1 :dc 1} {:dr 2 :dc 2} {:dr 3 :dc 3} {:dr 4 :dc 4} {:dr 5 :dc 5} {:dr 6 :dc 6} {:dr 7 :dc 7} {:dr 8 :dc 8}] (make-diffs {:dr 1 :dc 1})))))
 
 (deftest make-coords-1
   (testing "make-coords works"
-    (is (= [{ :row 2 :col 2} { :row 3 :col 3}] (make-coords { :row 1 :col 1} [{ :dr 1 :dc 1} { :dr 2 :dc 2}])))))
+    (is (= [{:row 2 :col 2} {:row 3 :col 3}] (make-coords {:row 1 :col 1} [{:dr 1 :dc 1} {:dr 2 :dc 2}])))))
 
 (deftest get-valid-coords-in-dir-1
   (testing "get-valid-coords-in-dir works"
-    (is (= [{ :row 2 :col 2} { :row 3 :col 3} { :row 4 :col 4} { :row 5 :col 5} { :row 6 :col 6} { :row 7 :col 7}] (get-valid-coords-in-dir { :row 1 :col 1} { :dr 1 :dc 1})))))
+    (is (= [{:row 2 :col 2} {:row 3 :col 3} {:row 4 :col 4} {:row 5 :col 5} {:row 6 :col 6} {:row 7 :col 7}] (get-valid-coords-in-dir {:row 1 :col 1} {:dr 1 :dc 1})))))
 
 (deftest count-stables-1
   (testing "simple case for count-stables"
@@ -37,27 +43,38 @@
   (testing "simple case for count-stables"
     (is (= 0 (count-stables [black white] white)))))
 
+(def corners-test [{:move {:row 0 :col 0}} {:move {:row 7 :col 7}}])
+(def sides-test [{:move {:row 0 :col 3}}])
+(def find-by-moves (concat corners-test sides-test))
+
+(deftest find-by-square-type-test
+  (testing "find by square type for corners works"
+    (is (= corners-test ((find-by-square-type :corner) find-by-moves)))))
+
+(deftest find-by-square-type-test
+  (testing "find by square type for sides works"
+    (is (= sides-test ((find-by-square-type :side) find-by-moves)))))
+
 (def test-find-stables-grid
   (make-grid [[0 0 black] [1 1 black] [0 1 black] [0 2 black] [1 0 black]]))
 
-
 (def test-grid-corner (make-grid [[0 0 black]]))
 
-(def get-value-test-1
+(deftest get-value-test-1
   (testing "get-value different for corner exists vs not"
-    (is (> (get-value test-grid-corner { :row 1 :col 1} ) (get-value start-grid { :row 1 :col 1})))))
+    (is (> (get-value test-grid-corner {:row 1 :col 1}) (get-value start-grid {:row 1 :col 1})))))
 
-(def max-by-test-3
+(deftest max-by-test-3
   (testing "max-by works for empty coll"
     (is (= nil (max-by (partial reduce +) [])))))
 
-(def max-by-test-2
+(deftest max-by-test-2
   (testing "max-by works for sum"
-    (is (= [10 2] (max-by (partial reduce +) [[10 2] [1 2 3] [] [1 1]])))))
+    (is (= [[10 2] [12]] (max-by (partial reduce +) [[10 2] [12] [1 2 3] [] [1 1]])))))
 
-(def max-by-test-1
+(deftest max-by-test-1
   (testing "max-by works for count"
-    (is (= [1 2 3] (max-by count [[1 2] [1 2 3] [] [1 1]])))))
+    (is (= [[1 2 3]] (max-by count [[1 2] [1 2 3] [] [1 1]])))))
 
 (deftest quick-test
   (testing "dirs size is 8"
@@ -70,18 +87,18 @@
   (testing "scores returns correct result"
     (is (= {white 2 black 2} (scores start-grid)))))
 (def next-grid  {{:row 3, :col 3} \B, {:row 4, :col 4} \B, {:row 3, :col 4} \B, {:row 4, :col 3} \W, {:row 2, :col 4} \B})
-(defn fst [mvs grd] (first mvs))
+(defn fst [mvs grd] [(first mvs)])
 (deftest play-next-move-1
   (testing "play-next-move works"
-    (is  (= (play-next-move start-grid black fst) next-grid))))
+    (is  (= (play-next-move start-grid black fst identity) next-grid))))
 
 (deftest get-all-moves-1
   (testing "get-all-moves works"
     (is (= (set (get-all-moves start-grid white))
-           #{[{ :row 2 :col 3} [{ :row 3 :col 3}]]
-             [{ :row 3 :col 2} [{ :row 3 :col 3}]]
-             [{ :row 4 :col 5} [{ :row 4 :col 4}]]
-             [{ :row 5 :col 4} [{ :row 4 :col 4}]]}))))
+           #{{:move {:row 2 :col 3} :color white :flips [{:row 3 :col 3}]}
+             {:move {:row 3 :col 2} :color white :flips [{:row 3 :col 3}]}
+             {:move {:row 4 :col 5} :color white :flips [{:row 4 :col 4}]}
+             {:move {:row 5 :col 4} :color white :flips [{:row 4 :col 4}]}}))))
 
 (def count-flips-1
   (testing
@@ -107,53 +124,53 @@
   (testing
    (is (= 4 (count-flips [black black black black white] white)))))
 
-(def new-grid (make-move start-grid [{ :row 2 :col 3} [{ :row 3 :col 3}]] white))
+(def new-grid (make-move start-grid {:move {:row 2 :col 3} :color white :flips [{:row 3 :col 3}]}))
 (deftest get-all-moves-2
   (testing "get-all-moves works against other case"
     (is (= (set (get-all-moves new-grid black))
-           #{[{ :row 2 :col 2} [{ :row 3 :col 3}]]
-             [{ :row 2 :col 4} [{ :row 3 :col 4}]]
-             [{ :row 4 :col 2} [{ :row 4 :col 3}]]}))))
-(def move-start {                         {:row 0 :col 1} white
+           #{{:color black :move {:row 2 :col 2} :flips [{:row 3 :col 3}]}
+             {:color black :move {:row 2 :col 4} :flips [{:row 3 :col 4}]}
+             {:color black :move {:row 4 :col 2} :flips [{:row 4 :col 3}]}}))))
+(def move-start {{:row 0 :col 1} white
                  {:row 1 :col 0} white {:row 1 :col 1} white})
 
 (def move-end {{:row 0 :col 0} black {:row 0 :col 1} black
                {:row 1 :col 0} black {:row 1 :col 1} black})
 (deftest make-move-1
   (testing "make-move returns correct results"
-    (is (= (make-move move-start [{ :row 0 :col 0} [{ :row 0 :col 1} { :row 1 :col 0} { :row 1 :col 1}]] black) move-end))))
+    (is (= (make-move move-start {:color black :move {:row 0 :col 0} :flips [{:row 0 :col 1} {:row 1 :col 0} {:row 1 :col 1}]}) move-end))))
 
 (deftest blank-neighbors-1
   (testing "blank-neighbors and neighbors return same result"
-    (is (= (blank-neighbors blank-grid { :row 3 :col 3}) (neighbors { :row 3 :col 3})))))
+    (is (= (blank-neighbors blank-grid {:row 3 :col 3}) (neighbors {:row 3 :col 3})))))
 
 (deftest blank-neighbors-2
   (testing "blank-neighbors and neighbors returns different result"
-    (is (not= (blank-neighbors start-grid { :row 3 :col 3}) (neighbors { :row 3 :col 3})))))
+    (is (not= (blank-neighbors start-grid {:row 3 :col 3}) (neighbors {:row 3 :col 3})))))
 
 (deftest blank-neighbors-3
   (testing "blank-neighbors returns correct result"
-    (is (= (blank-neighbors start-grid { :row 3 :col 3})) [{ :row 2 :col 2} { :row 2 :col 3} { :row 2 :col 4} { :row 3 :col 2} { :row 4 :col 2}])))
+    (is (= (blank-neighbors start-grid {:row 3 :col 3})) [{:row 2 :col 2} {:row 2 :col 3} {:row 2 :col 4} {:row 3 :col 2} {:row 4 :col 2}])))
 
 (deftest find-color-1
   (testing "find-color returns correct results"
-    (is (= (find-color start-grid black) [{ :row 3 :col 3} { :row 4 :col 4}]))))
+    (is (= (find-color start-grid black) [{:row 3 :col 3} {:row 4 :col 4}]))))
 
 (deftest find-color-2
   (testing "find-color returns correct results"
-    (is (= (find-color start-grid white) [{ :row 3 :col 4} { :row 4 :col 3}]))))
+    (is (= (find-color start-grid white) [{:row 3 :col 4} {:row 4 :col 3}]))))
 
 (deftest neighbors-1
   (testing "neighbors on corner"
-    (is (= 3 (count (neighbors { :row 0 :col 0}))))))
+    (is (= 3 (count (neighbors {:row 0 :col 0}))))))
 
 (deftest neighbors-2
   (testing "neighbors in middle"
-    (is (= 8 (count (neighbors { :row 3 :col 3}))))))
+    (is (= 8 (count (neighbors {:row 3 :col 3}))))))
 
 (deftest neighbors-3
   (testing "neighbors in edge"
-    (is (= 5 (count (neighbors { :row 3 :col 0}))))))
+    (is (= 5 (count (neighbors {:row 3 :col 0}))))))
 
 (deftest in-range-1
   (testing "inrange fails"
@@ -165,12 +182,28 @@
 
 (deftest in-range-coord-1
   (testing "inrange-coord fails"
-    (is (= false (in-range-coord? { :row 8 :col 3})))))
+    (is (= false (in-range-coord? {:row 8 :col 3})))))
 
 (deftest in-range-coord-2
   (testing "inrange-coord succeeds"
-    (is (= true (in-range-coord? { :row 3 :col 3})))))
+    (is (= true (in-range-coord? {:row 3 :col 3})))))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(def last-move-chooser-grid
+  (str-to-grid (apply str
+                      ["--------"
+                       "----B---"
+                       "----WWB-"
+                       "---BW---"
+                       "----BBB-"
+                       "--------"
+                       "--------"
+                       "--------"])))
+
+(def last-move-possible-moves (get-all-moves last-move-chooser-grid black))
+(deftest possible-moves-test-99
+  (testing "get-all-moves for last-chooser-grid"
+    (is (= 4 (count last-move-possible-moves)))))
+
+(deftest last-move-chooser-test
+  (testing "last-move-chooser works for simple case"
+    (is (= 1 (count (last-move-chooser last-move-possible-moves last-move-chooser-grid))))))
